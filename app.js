ba43ab3ed0abb51e16c7dee7afda85eadae7a0c3
Nazif Kadiroglu <nazif.kadiroglu1@gmail.com>
2022-12-21 16:53:46 -0800

    Data will now be stored in database (MongoDB)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const https = require("https");
 const cors = require("cors");
-
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
+const requestModule = require("request");
 
 app.use(cors());
-
 app.get("/api/tokens", (request, response) => {
+  // Make a request to the Zoho CRM API to get the leads data
   const options = {
     hostname: "www.zohoapis.com",
     port: 443,
@@ -14,25 +15,94 @@ app.get("/api/tokens", (request, response) => {
     method: "GET",
     headers: {
       Authorization:
-        "Zoho-oauthtoken 1000.9b2d0b451dc00cddd412128a289e9d6a.6ae62da2e791710ce449438b444bd584",
+        "Zoho-oauthtoken 1000.83e67e919a870af9a5e0bf76814bf48b.7314f9d99db35d09adbca4c78cee07e3",
     },
   };
-
   const req = https.request(options, (res) => {
     let data = "";
     res.on("data", (chunk) => {
       data += chunk;
     });
     res.on("end", () => {
-      response.send(data);
+      // Parse the response data as JSON
+      const leads = JSON.parse(data);
+      // Extract the parcel_id, zip_code, and lead_status for each lead
+      const leadData = leads.data.map((lead) => ({
+        parcel_id: lead.Parcel_ID,
+        zip_code: lead.Zip_Code,
+        lead_status: lead.Lead_Status,
+      }));
+      // Connect to the MongoDB database
+      MongoClient.connect(
+        "mongodb+srv://NazifTheDon:Aareyuok.123@map.fzvcuyl.mongodb.net/?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err, client) => {
+          if (err) {
+            console.error(err);
+            response.send({ error: err });
+            return;
+          }
+          // Insert the lead data into the "leads" collection in the database
+          const db = client.db("map");
+          db.collection("leads").insertMany(leadData, (insertErr, result) => {
+            if (insertErr) {
+              console.error(insertErr);
+              response.send({ error: insertErr });
+              return;
+            }
+            console.log(
+              `Inserted ${result.insertedCount} documents into the "leads" collection`
+            );
+            // Iterate over the lead data and make a request to the reportallusa API for each lead
+            leadData.forEach((lead) => {
+              if (lead.parcel_id !== null || lead.zip_code !== null) {
+                console.log(lead.parcel_id);
+                console.log(lead.zip_code);
+                // Use the request module to make a request to the reportallusa API
+                requestModule(
+                  `https://reportallusa.com/api/parcels?client=FXgevQoNSg&v=5&region=${lead["Zip_Code"]}&parcel_id=${lead["Parcel_ID"]}`,
+                  (error, reportallusaRes, body) => {
+                    if (error) {
+                      console.error(error);
+                      return;
+                    }
+                    console.log(body);
+                    // Extract the latitude and longitude from the response
+                    let lat = parseFloat(
+                      JSON.parse(body)["results"][0]["latitude"]
+                    );
+                    let lng = parseFloat(
+                      JSON.parse(body)["results"][0]["longitude"]
+                    );
+                    // Update the lead document in the database with the latitude and longitude
+                    db.collection("leads").updateOne(
+                      {
+                        parcel_id: lead.parcel_id,
+                        zip_code: lead.zip_code,
+                        lead_status: lead.lead_status,
+                      },
+                      { $set: { latitude: lat, longitude: lng } },
+                      (updateErr, updateResult) => {
+                        if (updateErr) {
+                          console.error(updateErr);
+                        } else {
+                          console.log(
+                            `Updated ${updateResult.modifiedCount} documents`
+                          );
+                        }
+                      }
+                    );
+                  }
+                );
+              }
+            });
+            client.close();
+          });
+          response.send({ message: "Successfully inserted lead data" });
+        }
+      );
     });
   });
-
-  req.on("error", (error) => {
-    console.error(error);
-    response.send(error);
-  });
-
   req.end();
 });
 
--- parcelID.js
+++ parcelID.js
@@ -1,61 +1,45 @@
 let map;
-let info;
 
-async function getTokens() {
+async function getLeads() {
+  // Make a request to the backend API to get the lead data
   const response = await fetch("http://localhost:3000/api/tokens");
   const data = await response.json();
-  return data["data"];
+  console.log(data); // log the data to debug the issue
+  return data;
 }
 
 async function initMap() {
-  info = await getTokens();
-  //console.log(info[2]["Lead_Status"]);
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.052059, lng: -86.470642 },
-    zoom: 5,
-  });
-  for (let i = 0; i < 50; i++) {
-    if (info[i]["Parcel_ID"] !== null || info[i]["Zip_Code"] !== null) {
-      console.log(info[i]["Parcel_ID"]);
-      console.log(info[i]["Zip_Code"]);
-
-      const request = new XMLHttpRequest();
-      request.open(
-        "GET",
-        `https://reportallusa.com/api/parcels?client=FXgevQoNSg&v=5&region=${info[i]["Zip_Code"]}&parcel_id=${info[i]["Parcel_ID"]}`
-      );
-      request.send();
-      request.onload = () => {
-        console.log(request.response);
-        if (request.status === 200) {
-          //title = JSON.parse(request.response)["results"][0]["parcel_id"];
-          let lat = parseFloat(
-            JSON.parse(request.response)["results"][0]["latitude"]
-          );
-          let lng = parseFloat(
-            JSON.parse(request.response)["results"][0]["longitude"]
-          );
-
-          let markerOption = {
-            position: { lat: lat, lng: lng },
-            map: map,
-          };
-
-          if (info[i]["Lead_Status"] === "Contacted") {
-            markerOption.icon =
-              "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
-          } else if (info[i]["Lead_Status"] === "Left Voicemail") {
-            markerOption.icon =
-              "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
-          }
-
-          let marker = new google.maps.Marker(markerOption);
-          marker.setMap(map);
-        } else {
-          console.log(`error ${request.status}`);
+  // Get the lead data from the backend API
+  const leadData = await getLeads();
+  if (Array.isArray(leadData)) {
+    // check if leadData is an array
+    // Initialize the map with a default center and zoom level
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 40.052059, lng: -86.470642 },
+      zoom: 5,
+    });
+    // Iterate over the lead data and add a marker for each lead
+    leadData.forEach((lead) => {
+      if (lead.latitude && lead.longitude) {
+        // Set the marker options based on the lead status
+        let markerOption = {
+          position: { lat: lead.latitude, lng: lead.longitude },
+          map: map,
+        };
+        if (lead.lead_status === "Contacted") {
+          markerOption.icon =
+            "https://maps.google.com/mapfiles/ms/icons/green-dot.png";
+        } else if (lead.lead_status === "Left Voicemail") {
+          markerOption.icon =
+            "https://maps.google.com/mapfiles/ms/icons/blue-dot.png";
         }
-      };
-    }
+        // Add the marker to the map
+        let marker = new google.maps.Marker(markerOption);
+        marker.setMap(map);
+      }
+    });
+  } else {
+    console.error("leadData is not an array");
   }
 }
 
